b48559839dc08db18afb90fc58f8ec4e6de8fc27
nandana-23 <nandunandanak558@gmail.com>
2023-08-31 12:08:20 +0530

    hello

---
+++ shop/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import *
+
+
+# Create your views here.
+def store(request):
+    products = product.objects.all()
+    context = {'products': products}
+    return render(request, 'store.html', context)
+
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+
+
+    context = {'items': items , 'order': order }
+    return render(request, 'cart.html', context)
+
+
+def check(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'check.html', context)
